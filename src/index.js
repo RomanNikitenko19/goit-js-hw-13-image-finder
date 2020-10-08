c5637531b5fa051c8164c09e1feb7b9cdabac6b9
RomanNikitenko <68384950+RomanNikitenko19@users.noreply.github.com>
2020-10-08 12:44:30 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import css from "./css/style.css";
+import renderApi from'./js/renderApi.js';
+
+
---
+++ src/js/renderApi.js
@@ -0,0 +1,3 @@
+import rets from './refs.js';
+import apiService from './apiService.js';
+import template from '../template/template.hbs';
