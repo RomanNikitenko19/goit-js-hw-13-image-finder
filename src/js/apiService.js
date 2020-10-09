a18696f23ad682cba8966e7e537de631a5e94c12
RomanNikitenko <68384950+RomanNikitenko19@users.noreply.github.com>
2020-10-09 15:11:57 +0300

    finish1

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,7 +1,5 @@
 import refs from './refs.js';
 
-const API_key = '18623542-c3ea86fe133d4cad85931c408';
-const baseUrl =`https://pixabay.com/api/`;
 // let query = 'js';
 // let page = 1;
 // let perPage = 12;
@@ -10,13 +8,30 @@ export default {
      _query: '',
      page: 1,
      perPage: 12,
-     fetchImages() {
+     async  fetchImages() {
+          const API_key = '18623542-c3ea86fe133d4cad85931c408';
+          const baseUrl =`https://pixabay.com/api/`;
           let url = `${baseUrl}?image_type=photo&orientation=horizontal&q=${this._query}&page=${this.page}&per_page=${this.perPage}&key=${API_key}`;
-          return fetch(url)
-          .then(res => res.json())
+          console.log(url);
+
+          try {
+               const res = await fetch(url);
+               const getResponse = await res.json();
+               return getResponse.hits;
+          } catch (error) {
+               throw error
+          }
+
+
+          // return fetch(url)
+          // .then(res => res.json())
+
           // .catch(error => displayError(error))
           // .then(({hits}) => hits);
+          console.log(getResponse);
+
      },
+
      setPage() {
           return this.page ++;
      },
--- src/js/renderApi.js
+++ src/js/renderApi.js
@@ -2,12 +2,33 @@ import apiService from './apiService.js';
 import template from '../template/template.hbs';
 import refs from './refs.js';
 import debounce from 'lodash.debounce';
+import * as basicLightbox from 'basiclightbox'
+import '../../node_modules/basiclightbox/dist/basicLightbox.min.css';
+import '@babel/polyfill';
+
+refs.galleryList.addEventListener('click', (event) => {
+     if(event.target.nodeName === "IMG") {
+          let modelSrc = event.target.dataset.src;
+
+          const instance = basicLightbox.create(`
+          <div class="modal">
+          <button class="js-mod-btn">close</button>
+          <img src="${modelSrc}" alt="picture" class="js-mod-img">
+          </div>
+          `)
+          instance.show();
+     };
+     const btnModal = document.querySelector('.js-mod-btn');
+     const imageModal = document.querySelector('.js-mod-img');
+     console.log(imageModal);
+     console.dir(event.target);
+});
 
 refs.form.addEventListener(
      'input',
      debounce(event => {
           event.preventDefault();
-          // console.dir(event.target.value)
+          refs.galleryList.innerHTML = ``;
           apiService.query = event.target.value;
           renderApi();
           refs.input.value = '';
@@ -17,7 +38,7 @@ refs.form.addEventListener(
 // apiService.fetchImages().then(({ hits }) => console.log(hits));
 
 function renderApi() {
-     apiService.fetchImages().then(({ hits }) => renderImages(hits));
+     apiService.fetchImages().then((data) => renderImages(data));
 }
 
 const loadMoreBtn = document.createElement('button');
@@ -38,7 +59,23 @@ function renderImages(data) {
 loadMoreBtn.addEventListener('click', loadMore);
 
 function loadMore() {
+     // let curentStart = document.documentElement.offsetHeight;
+
      apiService.setPage()
-     apiService.fetchImages().then(({ hits }) => renderImages(hits))
+     apiService.fetchImages().then((data) => renderImages(data))
+     setTimeout(() => {
+
+          window.scrollTo({
+               top: document.documentElement.offsetHeight - 4000,
+               behavior: 'smooth'
+          });
+
+     }, 500);
+     // console.dir(document);
+     // // console.log(document.documentElement);
+     // console.log(document.documentElement.offsetHeight);
+
+     // console.log(window.outerHeight);
+     // console.log(window.innerHeight);
 }
 
